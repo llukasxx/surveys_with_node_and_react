edf402c04da6ba96ee0f8cadf08554e42951ac9e
Łukasz Gorzelany <lukas@ukaszs-MBP.home>
2018-02-18 20:11:10 +0100

    set up routes

---
+++ client/src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Header from "./Header";
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
