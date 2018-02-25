95b1a2960a0de6977bda0fe9857afd6686dcf5e9
Łukasz Gorzelany <lukas@ukaszs-MBP.home>
2018-02-25 12:24:13 +0100

    add stripe

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import * as actions from "./../actions";
 import Header from "./Header";
+import Landing from "./Landing";
 
-const App = () => {
-  return (
-    <div>
-      <BrowserRouter>
-        <Header />
-      </BrowserRouter>
-    </div>
-  );
-};
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
 
-export default App;
+export default connect(null, actions)(App);
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,14 @@
+import "materialize-css/dist/css/materialize.min.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
+import reduxThunk from "redux-thunk";
+
 import reducers from "./reducers";
 import App from "./components/App";
 
-const store = createStore(reducers, {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
   <Provider store={store}>
@@ -13,3 +16,5 @@ ReactDOM.render(
   </Provider>,
   document.querySelector("#root")
 );
+
+console.log(process.env.REACT_APP_STRIPE_KEY);
