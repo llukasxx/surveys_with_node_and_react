f8ad6047a4a608441da1dfb8421677d49a50fe3e
Łukasz Gorzelany <lukas@ukaszs-MBP.home>
2018-03-10 11:23:27 +0100

    add redux-form

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,6 +4,8 @@ import { connect } from "react-redux";
 import * as actions from "./../actions";
 import Header from "./Header";
 import Landing from "./Landing";
+import Dashboard from "./Dashboard";
+import SurveyNew from "./surveys/SurveyNew";
 
 class App extends Component {
   componentDidMount() {
@@ -13,9 +15,11 @@ class App extends Component {
     return (
       <div className="container">
         <BrowserRouter>
-          <div>
+          <div className="container">
             <Header />
             <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
           </div>
         </BrowserRouter>
       </div>
--- client/src/index.js
+++ client/src/index.js
@@ -7,8 +7,6 @@ import reduxThunk from "redux-thunk";
 
 import reducers from "./reducers";
 import App from "./components/App";
-import axios from "axios";
-window.axios = axios;
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
